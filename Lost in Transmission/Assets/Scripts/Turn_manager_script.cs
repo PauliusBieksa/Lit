fab7534c7bc6475f6460241a1a8c01a4909c5d44
Paulius Bieksa <priverstinis@gmail.com>
2018-01-26 23:28:57 +0000

    Started the turn manager script + made a prefab dor turn manager.

---
+++ Lost in Transmission/Assets/Scripts/Turn_manager_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn_manager_script : MonoBehaviour
+{
+    List<MoveType> moves;
+    int starting_index = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        moves = new List<MoveType>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void AddMove(MoveType m)
+    {
+
+    }
+}
