9fbbb35e128ae3ca0beff931af0be832d6bd9c8f
Dan's Laptop <dkorsah15@aol.co.uk>
2018-01-26 23:24:48 +0000

    Have the enums.

--- Lost in Transmission/Assets/Scripts/PlayerStates.cs
+++ Lost in Transmission/Assets/Scripts/PlayerStates.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class PlayerStates
+public enum Dirs
 {
+	N = 0,
+	NE = 45,
+	E = 90,
+	SE = 135,
+	S = 180,
+	SW = 225,
+	W = 270,
+	NW = 315
+}
 
-	public enum Orientation
-	{
-		N = 0,
-		NE = 45,
-		E = 90,
-		SE = 135,
-		S = 180,
-		SW = 225,
-		W = 270,
-		NW = 315
-	}
+public enum MoveTypes
+{
+	MOVE,
+	BLOCK,
+	MELEE,
+	RANGE
+};
 
+public struct Move
+{
+	public string Type;
+	public float Dir;
 }
