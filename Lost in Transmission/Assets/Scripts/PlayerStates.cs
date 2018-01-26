3a4bee4efa39fcdce4f575446272b4516768942f
Dan's Laptop <dkorsah15@aol.co.uk>
2018-01-26 21:46:38 +0000

    Pushing sln

---
+++ Lost in Transmission/Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates
+{
+
+	public enum Orientation
+	{
+		N = 0,
+		NE = 45,
+		E = 90,
+		SE = 135,
+		S = 180,
+		SW = 225,
+		W = 270,
+		NW = 315
+	}
+
+}
