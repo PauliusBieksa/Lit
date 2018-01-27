9e3279004ac1583603077ab43ba9fe6e3ba26026
Dan's Laptop <dkorsah15@aol.co.uk>
2018-01-27 05:33:34 +0000

    Perfected movement.

--- Lost in Transmission/Assets/Scripts/PlayerController.cs
+++ Lost in Transmission/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
 	Vector3 pos;
 	Quaternion rot;
 	Transform trans;
+	bool rotating;
 	public float step = 2.0f;
 	public float waitTime = 1.0f;
 
 	public Move m1;
 	public Move m2;
+	public Move m3;
 	List<Move> m = new List<Move> ();
 
 	void Start ()
@@ -26,8 +28,12 @@ public class PlayerController : MonoBehaviour
 		m2.dir = Dirs.S;
 		m2.type = MoveTypes.MOVE;
 
+		m3.dir = Dirs.NE;
+		m3.type = MoveTypes.MOVE;
+
 		m.Add (m1);
 		m.Add (m2);
+		m.Add (m3);
 
 		Debug.Log ("Started");
 	}
@@ -56,7 +62,7 @@ public class PlayerController : MonoBehaviour
 				StartCoroutine (Turn (mov));
 				yield return new WaitForSeconds (waitTime);
 				Debug.Log ("turned");
-				Translate (mov);
+				StartCoroutine (Translate (mov));
 				yield return new WaitForSeconds (waitTime);
 				Debug.Log ("moved");
 
@@ -64,8 +70,9 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-	void Translate (Move mov)
+	IEnumerator Translate (Move mov)
 	{
+		Vector3 startPos = pos;
 		float end;
 		if (mov.dir == Dirs.N || mov.dir == Dirs.E || mov.dir == Dirs.S || mov.dir == Dirs.W)
 		{
@@ -75,22 +82,35 @@ public class PlayerController : MonoBehaviour
 		{
 			end = Mathf.Sqrt (2);
 		}
-		pos += transform.forward * end * Time.deltaTime;
-		gameObject.transform.position = pos;
-		Debug.Log ("transform by " + transform.forward * end * Time.deltaTime);
+
+		Debug.Log ("transform by " + transform.forward * end);
+
+		while (rotating)
+		{
+			yield return null;
+		}
+
+		while ((transform.position - startPos).magnitude != end)
+		{
+			pos += transform.up * end;
+			transform.position = pos;
+			//gameObject.transform.position = pos;
+			yield return new WaitForSeconds (waitTime);
+		}
+
 	}
 
 	IEnumerator Turn (Move mov)
 	{
-		Debug.Log ("Rotating");
 		Debug.Log ("rotate to " + (float) mov.dir + " " + mov.dir);
 		Quaternion target = Quaternion.Euler (0, 0, (float) mov.dir);
 		while (rot != target)
 		{
+			rotating = true;
 			rot = Quaternion.RotateTowards (rot, target, step);
 			gameObject.transform.rotation = rot;
 			yield return null;
 		}
-
+		rotating = false;
 	}
 }
