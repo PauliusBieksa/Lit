4d84226161556b89b1a1a9462f5b8ea4496e04f1
Dan's Laptop <dkorsah15@aol.co.uk>
2018-01-27 20:58:26 +0000

    my junk

--- Lost in Transmission/Assets/Scripts/JoinGame.cs
+++ Lost in Transmission/Assets/Scripts/JoinGame.cs
@@ -6,7 +6,9 @@ public class JoinGame : MonoBehaviour
 {
 
 	[SerializeField]
-	PlayerInput pI;
+	GameObject[] player = new GameObject[2];
+
+	PlayerInput[] pI = new PlayerInput[2];
 
 	RectTransform rect;
 	public float speed = 30;
@@ -16,36 +18,47 @@ public class JoinGame : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		player[0].GetComponent<SpriteRenderer> ().sortingOrder = -5;
+		player[1].GetComponent<SpriteRenderer> ().sortingOrder = -5;
 		rect = gameObject.GetComponent<RectTransform> ();
+		pI[0] = player[0].GetComponent<PlayerInput> ();
+		pI[1] = player[1].GetComponent<PlayerInput> ();
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		if (pI.ButtonDown (Button.A))
+		if (pI[0].ButtonDown (Button.A))
 		{
-			StartCoroutine (slide ());
+			StartCoroutine (slideL ());
+		}
+
+		if (pI[1].ButtonDown (Button.A))
+		{
+			StartCoroutine (slideR ());
 		}
 	}
 
-	IEnumerator slide ()
+	IEnumerator slideL ()
 	{
-		while (rect.position != Left || rect.position != Right)
+		while (rect.position != Left)
 		{
 			float before = rect.position.y;
 
-			if (gameObject.name.Contains ("Red"))
-			{
-				rect.position = Vector3.MoveTowards (rect.position, Left, speed);
-				Instantiate (pI.gameObject);
-				pI.gameObject.transform.position = new Vector3 (-2.5f, 0.5f, 0);
-			}
-			else
-			{
-				rect.position = Vector3.MoveTowards (rect.position, Right, speed);
-				Instantiate (pI.gameObject);
-				pI.gameObject.transform.position = new Vector3 (2.5f, 0.5f, 0);
-			}
+			rect.position = Vector3.MoveTowards (rect.position, Left, speed);
+			player[0].GetComponent<SpriteRenderer> ().sortingOrder = 1;
+			pI[0].gameObject.transform.position = new Vector3 (-2.5f, 0.5f, 0);
+			yield return null;
+		}
+	}
+
+	IEnumerator slideR ()
+	{
+		while (rect.position != Right)
+		{
+			rect.position = Vector3.MoveTowards (rect.position, Right, speed);
+			player[1].GetComponent<SpriteRenderer> ().sortingOrder = 1;
+			pI[1].gameObject.transform.position = new Vector3 (2.5f, 0.5f, 0);
 			yield return null;
 		}
 	}
