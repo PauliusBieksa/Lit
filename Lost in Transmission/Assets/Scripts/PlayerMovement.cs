4b7e949bf787940440fa93812d1fe3e521c06159
Tim-Surface <40219124@live.napier.ac.uk>
2018-01-27 08:07:30 +0000

    Other buttons test - works most of the time.

--- Lost in Transmission/Assets/Scripts/PlayerMovement.cs
+++ Lost in Transmission/Assets/Scripts/PlayerMovement.cs
@@ -28,6 +28,22 @@ public class PlayerMovement : MonoBehaviour
             outVec = pI.JoystickInput();
             transform.Translate(Quaternion.Euler(0.0f, 0.0f, 360 - (int)pI.CompassInput()) * new Vector3(0.0f, 1.0f, 0.0f) * Time.fixedDeltaTime);
         }
+        if (pI.ButtonDown(Button.A))
+        {
+            transform.Translate(new Vector3(0.0f, -1.0f, 0.0f));
+        }
+        if (pI.ButtonDown(Button.Y))
+        {
+            transform.Translate(new Vector3(0.0f, 1.0f, 0.0f));
+        }
+        if (pI.ButtonDown(Button.B))
+        {
+            transform.Translate(new Vector3(1.0f, 0.0f, 0.0f));
+        }
+        if (pI.ButtonDown(Button.X))
+        {
+            transform.Translate(new Vector3(-1.0f, 0.0f, 0.0f));
+        }
         //transform.Translate(pI.JoystickInput() * Time.fixedDeltaTime * 10.0f);
     }
 }
