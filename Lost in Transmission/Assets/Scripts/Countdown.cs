850bb2f5eb0c3c2609399dd98fba19d293a4023d
Dan's Laptop <dkorsah15@aol.co.uk>
2018-01-28 14:26:54 +0000

    Countdown timer prefab and script.

---
+++ Lost in Transmission/Assets/Scripts/Countdown.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+
+	void CountdownUpdate (float maxSecs, float secondsRemaining)
+	{
+		float percent = secondsRemaining / maxSecs;
+		gameObject.transform.localScale = new Vector3 (percent, percent, percent);
+	}
+
+}
