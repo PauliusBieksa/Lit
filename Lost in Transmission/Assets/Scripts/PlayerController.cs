a77dc0d5f0a66aae8567e55c57a83e1dafd2e27b
Dan's Laptop <dkorsah15@aol.co.uk>
2018-02-01 16:26:30 +0000

    Revert "Revert "desperate times""
    This reverts commit 87d7c0f3f5d141f99373f1261d5e19529b914669.

--- Lost in Transmission/Assets/Scripts/PlayerController.cs
+++ Lost in Transmission/Assets/Scripts/PlayerController.cs
@@ -4,180 +4,187 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //Vector3 pos;
-    //Quaternion rot;
-    //Transform trans;
+	Vector3 pos;
+	Quaternion rot;
+	Transform trans;
 
-    //public float step = 2.0f;
-    //public float waitTime = 1.0f;
-    //public float dist = 1;
-    //public float chargePower = 2;
+	public float step = 2.0f;
+	public float waitTime = 1.0f;
+	public float dist = 1;
+	public float chargePower = 2;
 
-    //bool rotating;
-    //bool moving;
-    //bool collidable = false;
+	bool rotating;
+	bool moving;
+	bool collidable = false;
 
-    //public float startTime;
-    //public float journeyTime = 0.5f;
+	public float startTime;
+	public float journeyTime = 0.5f;
 
-    //[SerializeField] public Move m1;
-    //[SerializeField] public Move m2;
-    //[SerializeField] public Move m3;
-    //List<Move> m = new List<Move>();
-    //public static Move mov;
+	[SerializeField] public Move m1;
+	[SerializeField] public Move m2;
+	[SerializeField] public Move m3;
+	List<Move> m = new List<Move> ();
+	public static Move mov;
 
-    //[SerializeField] Turn_manager_script tm;
-    public List<Move> Passthrough = new List<Move>();
-    public bool Executed = false;
+	[SerializeField] Turn_manager_script tm;
+	public List<Move> Passthrough = new List<Move> ();
+	public bool Executed = false;
 
-    //BoxCollider2D col;
+	//BoxCollider2D col;
 
-    void Start()
-    {
-        //pos = gameObject.transform.position;
-        //rot = gameObject.transform.rotation;
-        //trans = gameObject.transform;
+	void Start ()
+	{
+		pos = gameObject.transform.position;
+		rot = gameObject.transform.rotation;
+		trans = gameObject.transform;
 
-        //m1.dir = Dirs.E;
-        //m1.type = MoveTypes.NONE;
+		m1.dir = Dirs.E;
+		m1.type = MoveTypes.MOVE;
 
-        //m2.dir = Dirs.S;
-        //m2.type = MoveTypes.NONE;
+		m2.dir = Dirs.S;
+		m2.type = MoveTypes.MOVE;
 
-        //m3.dir = Dirs.NW;
-        //m3.type = MoveTypes.NONE;
+		m3.dir = Dirs.NW;
+		m3.type = MoveTypes.MELEE;
 
-        //m.Add(m1);
-        //m.Add(m2);
-        //m.Add(m3);
+		m.Add (m1);
+		m.Add (m2);
+		m.Add (m3);
 
-        //col = gameObject.GetComponent<BoxCollider2D> ();
+		//col = gameObject.GetComponent<BoxCollider2D> ();
 
-    }
+	}
 
-    //public void TurnPassthrough(List<Move> p)
-    //{
-    //    Passthrough = p;
-    //}
+	public void TurnPassthrough (List<Move> p)
+	{
+		Passthrough = p;
+	}
 
-    //public void ExecuteMovesSequence(List<Move> m)
-    //{
-    //    //StartCoroutine(ExecuteMoves(m));
-    //}
+	public void ExecuteMovesSequence (List<Move> m)
+	{
+		StartCoroutine (ExecuteMoves (m));
+	}
 
-    // Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //    {
-    //        //StartCoroutine(ExecuteMoves(m));
-    //    }
-    //}
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			StartCoroutine (ExecuteMoves (m));
+		}
+	}
 
-    public void ExecuteMoves(List<Move> moves)
-    {
-        Passthrough = moves;
-        // if (mov.type == MoveTypes.BLOCK)
-        // {
-        // 	Vector3 reset = transform.position;
-        // 	yield return new WaitForSeconds (0.6f);
-        // 	transform.position = reset;
-        // }
-        //if (mov.type == MoveTypes.MOVE)
-        //{
-        //	StartCoroutine (Turn (mov));
-        //	yield return new WaitForSeconds (waitTime);
-        //	StartCoroutine (Translate (mov, dist, gameObject));
-        //   yield return new WaitForSeconds(waitTime);
-        //  }
-        // else if (mov.type == MoveTypes.MELEE)
-        // {
-        // 	StartCoroutine (Turn (mov));
-        // 	yield return new WaitForSeconds (waitTime);
-        // 	Debug.Log ("turned");
-        // 	StartCoroutine (Translate (mov, chargePower, gameObject));
-        // 	collidable = true;
+	public IEnumerator ExecuteMoves (List<Move> moves)
+	{
+		Executed = false;
+		int i = 0;
+		foreach (Move m in moves)
+		{
+			mov = m;
+			i++;
 
-        // }
-        Executed = true;
-        //tm.Start();
-    }
+			// if (mov.type == MoveTypes.BLOCK)
+			// {
+			// 	Vector3 reset = transform.position;
+			// 	yield return new WaitForSeconds (0.6f);
+			// 	transform.position = reset;
+			// }
+			if (mov.type == MoveTypes.MOVE)
+			{
+				StartCoroutine (Turn (mov));
+				yield return new WaitForSeconds (waitTime);
+				StartCoroutine (Translate (mov, dist, gameObject));
 
-    //void OnCollisionEnter(Collision other)
-    //{
-    //    collidable = false;
-    //    if (other.gameObject.name.ToLower().Contains("player"))
-    //    {
-    //        StopCoroutine(Translate(mov, chargePower, gameObject));
-    //        Pushback(other.gameObject, mov);
-    //    }
-    //}
+			}
+			// else if (mov.type == MoveTypes.MELEE)
+			// {
+			// 	StartCoroutine (Turn (mov));
+			// 	yield return new WaitForSeconds (waitTime);
+			// 	Debug.Log ("turned");
+			// 	StartCoroutine (Translate (mov, chargePower, gameObject));
+			// 	collidable = true;
 
-    //public void Pushback(GameObject hitPlayer, Move m)
-    //{
-    //    StartCoroutine(Translate(mov, chargePower, hitPlayer));
-    //}
+			// }
+		}
 
-    //IEnumerator Translate(Move mov, float d, GameObject g)
-    //{
-    //    Vector3 startPos = transform.position;
-    //    float end;
-    //    if (mov.dir == Dirs.N || mov.dir == Dirs.E || mov.dir == Dirs.S || mov.dir == Dirs.W)
-    //    {
-    //        end = d;
-    //    }
-    //    else
-    //    {
-    //        end = Mathf.Sqrt((d * d) + (d * d));
-    //    }
+		//tm.Start();
+	}
 
-    //    while (rotating || moving)
-    //    {
-    //        yield return null;
-    //    }
+	void OnCollisionEnter (Collision other)
+	{
+		collidable = false;
+		if (other.gameObject.name.ToLower ().Contains ("player"))
+		{
+			StopCoroutine (Translate (mov, chargePower, gameObject));
+			Pushback (other.gameObject, mov);
+		}
+	}
 
-    //    startTime = Time.time;
-    //    float fracComplete = 0.0f;
-    //    while (fracComplete < 1.0f)
-    //    {
-    //        float elapsedTime = (Time.time - startTime);
-    //        float localDT = Time.deltaTime / journeyTime;
-    //        if ((elapsedTime + localDT) > journeyTime)
-    //        {
-    //            localDT = journeyTime - elapsedTime;
-    //        }
-    //        fracComplete = elapsedTime / journeyTime;
-    //        moving = true;
+	public void Pushback (GameObject hitPlayer, Move m)
+	{
+		StartCoroutine (Translate (mov, chargePower, hitPlayer));
+	}
 
-    //        pos += (transform.up * end * localDT);
-    //        transform.position = (pos);
-    //        //gameObject.transform.position = pos;
-    //        yield return null;
-    //    }
-    //    pos.x = Mathf.Floor(pos.x) + 0.5f;
-    //    pos.y = Mathf.Floor(pos.y) + 0.5f;
-    //    transform.position = pos;
-    //    moving = false;
-    //    yield return new WaitForSeconds(waitTime);
+	IEnumerator Translate (Move mov, float d, GameObject g)
+	{
+		Vector3 startPos = transform.position;
+		float end;
+		if (mov.dir == Dirs.N || mov.dir == Dirs.E || mov.dir == Dirs.S || mov.dir == Dirs.W)
+		{
+			end = d;
+		}
+		else
+		{
+			end = Mathf.Sqrt ((d * d) + (d * d));
+		}
 
-    //}
+		while (rotating || moving)
+		{
+			yield return null;
+		}
 
-    //IEnumerator Turn(Move mov)
-    //{
-    //    Quaternion target = Quaternion.Euler(0, 0, (float)mov.dir);
+		startTime = Time.time;
+		float fracComplete = 0.0f;
+		while (fracComplete < 1.0f)
+		{
+			float elapsedTime = (Time.time - startTime);
+			float localDT = Time.deltaTime / journeyTime;
+			if ((elapsedTime + localDT) > journeyTime)
+			{
+				localDT = journeyTime - elapsedTime;
+			}
+			fracComplete = elapsedTime / journeyTime;
+			moving = true;
 
-    //    while (rotating || moving)
-    //    {
-    //        yield return null;
-    //    }
+			pos += (transform.up * end * localDT);
+			transform.position = (pos);
+			//gameObject.transform.position = pos;
+			yield return null;
+		}
+		pos.x = Mathf.Floor (pos.x) + 0.5f;
+		pos.y = Mathf.Floor (pos.y) + 0.5f;
+		transform.position = pos;
+		moving = false;
+		yield return new WaitForSeconds (waitTime);
 
-    //    while (rot != target)
-    //    {
-    //        rotating = true;
-    //        rot = Quaternion.RotateTowards(rot, target, step);
-    //        gameObject.transform.rotation = rot;
-    //        yield return null;
-    //    }
-    //    rotating = false;
-    //}
+	}
+
+	IEnumerator Turn (Move mov)
+	{
+		Quaternion target = Quaternion.Euler (0, 0, (float) mov.dir);
+
+		while (rotating || moving)
+		{
+			yield return null;
+		}
+
+		while (rot != target)
+		{
+			rotating = true;
+			rot = Quaternion.RotateTowards (rot, target, step);
+			gameObject.transform.rotation = rot;
+			yield return null;
+		}
+		rotating = false;
+	}
 }
